51aef691d88fe01bb8ab8b789aaad34e1ac1d901
Prof. Fernando Pinéo <pf1963@fiap.com.br>
2025-08-18 09:55:18 -0300

    Criado o botão para excluir a conta na Tela HomeScreen e criado a tela AlterarSenha, faltar a partir da parte de reautenticar o usuário

---
+++ app/AlterarSenha.tsx
@@ -0,0 +1,120 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Alert } from 'react-native';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../services/firebaseConfig'
+import { useRouter } from 'expo-router';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+export default function CadastroScreen() {
+  // Estados para armazenar os valores digitados
+  const [novaSenha, setNovaSenha] = useState('');
+  const [confirmarSenha, setConfirmarSenha] = useState('');
+  const [senhaAtual, setSenhaAtual] = useState('');
+
+  const router = useRouter() // Hook para navegação
+
+  // Função para realizar update de senha
+  const handleAlterarSenha = () => {
+    if (!novaSenha || !confirmarSenha || !senhaAtual) {
+      Alert.alert('Atenção', 'Preencha todos os campos!');
+      return;
+    }
+    //Verificar se a nova senha é igual a confirmação da senha
+    if(novaSenha!== confirmarSenha){
+        Alert.alert("Erro","As senhas não coincidem!")
+        return
+    }
+    //Tratamento do tamanho da senha se for menos 6 caracteres
+    if(novaSenha.length<6){
+        Alert.alert("Erro","A nova senha deve ter pelo menos 6 caracteres.")
+        return
+    }
+    try{
+        const user = auth.currentUser;
+        if(!user || !user.email){
+            Alert.alert("Erro","Nenhum usário logado.")
+            return
+        }
+        //Cria as credenciais com e-mail e senha atual para reatenticar
+        
+    }
+
+    
+    }
+
+    return (
+      <View style={styles.container}>
+        <Text style={styles.titulo}>Criar Conta</Text>
+
+        {/* Campo Nome */}
+        <TextInput
+          style={styles.input}
+          placeholder="Digite a senha atual"
+          placeholderTextColor="#aaa"
+          value={senhaAtual}
+          onChangeText={setSenhaAtual}
+        />
+
+        {/* Campo Email */}
+        <TextInput
+          style={styles.input}
+          placeholder="Digite a nova senha"
+          placeholderTextColor="#aaa"
+          value={novaSenha}
+          onChangeText={setNovaSenha}
+        />
+
+        {/* Campo Senha */}
+        <TextInput
+          style={styles.input}
+          placeholder="Confirme a nova senha"
+          placeholderTextColor="#aaa"
+          value={confirmarSenha}
+          onChangeText={setConfirmarSenha}
+        />
+
+        {/* Botão */}
+        <TouchableOpacity style={styles.botao} onPress={handleAlterarSenha}>
+          <Text style={styles.textoBotao}>Alterar Senha</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
+  // Estilização
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#121212',
+      justifyContent: 'center',
+      padding: 20,
+    },
+    titulo: {
+      fontSize: 28,
+      fontWeight: 'bold',
+      color: '#fff',
+      marginBottom: 30,
+      textAlign: 'center',
+    },
+    input: {
+      backgroundColor: '#1E1E1E',
+      color: '#fff',
+      borderRadius: 10,
+      padding: 15,
+      marginBottom: 15,
+      fontSize: 16,
+      borderWidth: 1,
+      borderColor: '#333',
+    },
+    botao: {
+      backgroundColor: '#00B37E',
+      padding: 15,
+      borderRadius: 10,
+      alignItems: 'center',
+    },
+    textoBotao: {
+      color: '#fff',
+      fontSize: 18,
+      fontWeight: 'bold',
+    },
+  });
--- app/HomeScreen.tsx
+++ app/HomeScreen.tsx
@@ -1,7 +1,10 @@
-import { Text,Button } from "react-native";
+import { Text,Button,Alert } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useRouter } from "expo-router";
+
+import {auth} from '../services/firebaseConfig'
+import { deleteUser } from "firebase/auth";
 
 export default function HomeScreen() {
     const router = useRouter()//Hook de navegação entre telas
@@ -9,10 +12,38 @@ export default function HomeScreen() {
         await AsyncStorage.removeItem('@user')
         router.push('/')
     }
+    const excluirConta = () =>{
+        Alert.alert(
+            "Confirmar Exclusão",
+            "Tem certeza que deseja excluir sua conta? Esta ação não poderá ser desfeita!",
+            [
+                {text:"Cancelar",style:"cancel"},
+                {text:"Excluir",style:'destructive',
+                    onPress:async()=>{
+                        try{
+                            const user = auth.currentUser
+                            if(user){
+                                await deleteUser(user)//Apaga do firebase Auth
+                                await AsyncStorage.removeItem('@user')
+                                Alert.alert("Conta Excluída", "Sua conta foi excluída com sucesso.")
+                                router.replace('/')
+                            }else{
+                                Alert.alert("Erro","Nenhum usuário logado.")
+                            }
+                        }catch(error){
+                            console.log("Erro ao excluir conta.")
+                            Alert.alert("Error", "Não foi possível excluir conta")
+                        }
+                    }
+                }
+            ]
+        )
+    }
     return (
         <SafeAreaView>
             <Text>Seja bem-vindo a Tela Inicial da Aplicação</Text>
             <Button title="Sair da Conta" onPress={realizarLogoff}/>
+            <Button title="Exluir conta" color='red' onPress={excluirConta}/>
         </SafeAreaView>
     )
 }
